8536a71e5167d5da681702a7ccd6d94e66570d93
Cameron Battagler <cameron@battagler.me>
2017-09-24 21:21:52 -0500

    Further work, initial working

--- booch_pi.py
+++ booch_pi.py
@@ -4,13 +4,15 @@ import time
 import Adafruit_DHT
 
 GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
 
 GPIO.setup(2, GPIO.OUT)
 GPIO.output(2, GPIO.HIGH)
 
 while True:
-    humidity, temperature = Adafruit_DHT.read_retry(22, 4)
-    print 'Temp: {0:0.1f} C Humidity: {1:0.1f} %'.format(temperature, humidity)
+    humidity, temperature = Adafruit_DHT.read_retry(22, 3)
+    temperature = (temperature*1.8)+32
+    print 'Temp: {0:0.1f} F Humidity: {1:0.1f} %'.format(temperature, humidity)
     if temperature < 76.5:    
         try:
             GPIO.output(2, GPIO.LOW)
