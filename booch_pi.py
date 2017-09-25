9053042e18ddce33b90e80ffbd2a5f8a55c78e26
Cameron Battagler <cameron@battagler.me>
2017-09-24 21:24:55 -0500

    Added cleanup on keyboard kill

--- booch_pi.py
+++ booch_pi.py
@@ -4,7 +4,6 @@ import time
 import Adafruit_DHT
 
 GPIO.setmode(GPIO.BCM)
-GPIO.cleanup()
 
 GPIO.setup(2, GPIO.OUT)
 GPIO.output(2, GPIO.HIGH)
@@ -13,17 +12,19 @@ while True:
     humidity, temperature = Adafruit_DHT.read_retry(22, 3)
     temperature = (temperature*1.8)+32
     print 'Temp: {0:0.1f} F Humidity: {1:0.1f} %'.format(temperature, humidity)
-    if temperature < 76.5:    
-        try:
-            GPIO.output(2, GPIO.LOW)
-            time.sleep(10)
-        except:
-            print "Failed to turn on Relay"
-    if temperature > 77.5:
-        try:
-            GPIO.output(2, GPIO.HIGH)
-            time.sleep(10)
-        except:
-            print "Failed to turn off Relay"
-    
-GPIO.cleanup()
+    try:
+        if temperature < 76.5:    
+            try:
+                GPIO.output(2, GPIO.LOW)
+                time.sleep(10)
+            except:
+                print "Failed to turn on Relay"
+        if temperature > 77.5:
+            try:
+                GPIO.output(2, GPIO.HIGH)
+                time.sleep(10)
+            except:
+                print "Failed to turn off Relay"
+    except KeyboardInterrupt:
+        print " Killing process"
+        GPIO.cleanup()
