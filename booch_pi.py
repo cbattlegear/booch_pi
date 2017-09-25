bc988e9c86d679702aacc213014d84e9227374f7
Cameron Battagler <cameron@battagler.me>
2017-09-24 20:55:46 -0500

    Initial attempt

--- booch_pi.py
+++ booch_pi.py
@@ -3,16 +3,25 @@ import RPi.GPIO as GPIO
 import time
 import Adafruit_DHT
 
-
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(2, GPIO.OUT)
 GPIO.output(2, GPIO.HIGH)
-while True:
 
-try:
-    GPIO.output(2, GPIO.LOW)
-    time.sleep(10)
-except KeyboardInterrupt:
-    print"Quit"
-    GPIO.cleanup()
+while True:
+    humidity, temperature = Adafruit_DHT.read_retry(22, 4)
+    print 'Temp: {0:0.1f} C Humidity: {1:0.1f} %'.format(temperature, humidity)
+    if temperature < 76.5:    
+        try:
+            GPIO.output(2, GPIO.LOW)
+            time.sleep(10)
+        except:
+            print "Failed to turn on Relay"
+    if temperature > 77.5:
+        try:
+            GPIO.output(2, GPIO.HIGH)
+            time.sleep(10)
+        except:
+            print "Failed to turn off Relay"
+    
+GPIO.cleanup()
